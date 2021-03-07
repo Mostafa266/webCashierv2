45492239cc4eda21f135eb056def679b587e3e14
DESKTOP-VC20UED\MostafaAbdelazeem <mostafa.abdo.ch12@gmail.com>
2021-03-07 07:02:12 +0200

    Upload files

---
+++ js/main.js
@@ -0,0 +1,56 @@
+$( document ).ready(function() {
+//Avoid dropdown menu close on click inside
+// $("#Dropdown").click(function(e){
+//   e.stopPropagation();
+// })
+//side menu overlay
+    $('#navbarSideButton').on('click', function() {
+      $('#navbarSide').addClass('reveal')
+    });
+      // Open navbarSide when button is clicked
+      $('#navbarSideButton').on('click', function() {
+        $('#navbarSide').addClass('reveal');
+        $('.overlay').show();
+      });
+    
+      // Close navbarSide when the outside of menu is clicked
+      $('.overlay').on('click', function(){
+        $('#navbarSide').removeClass('reveal');
+        $('.overlay').hide();
+      });
+
+  });
+      //focus on first input in add new client modal
+
+  $('#addNew_client').on('shown.bs.modal', function() {
+    $('client-name').focus();
+  })
+  function toggleClientsList(){
+    var x = document.getElementById("clientList");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  //toggle favourite icon 
+  function swap(){
+    let img1= document.getElementById('favauriteIcon1');
+      img2= document.getElementById('favauriteIcon2');
+      img1src=img1.src;
+      img1.style.display='none'
+      img2.style.display='block'
+    // img1.src="imgs/client_icon.svg"
+  }
+  function Toggleswap(){
+    let img1= document.getElementById('favauriteIcon1');
+    img2= document.getElementById('favauriteIcon2');
+    img1src=img1.src;
+    img1.style.display='block'
+    img2.style.display='none'
+  }
+
+
+  $('#lang').on('click',function(){
+    $('body').toggleClass('ltr')
+  })
