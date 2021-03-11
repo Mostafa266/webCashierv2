10ad5797615fc4c91b3e8a826f9349fa673e5227
DESKTOP-VC20UED\MostafaAbdelazeem <mostafa.abdo.ch12@gmail.com>
2021-03-11 02:01:58 +0200

    Add animation for dropdown menu

--- js/animation.js
+++ js/animation.js
@@ -16,9 +16,8 @@ $('#lang').on('click',function(){
 
 
 
-
+    // add client list animation
   let tl2 = new TimelineMax({paused:true});
-
   tl2.to('#productsList',0.3,{opacity:'0',display:'none'})
   tl2.fromTo('#clientList',0.3,{display:'none',opacity:'0'},{display:'block',opacity:1})
   tl2.fromTo('.headerAddClient',0.3,{opacity:0,top:'40px'},{opacity:1,top:"4px"})
@@ -33,22 +32,7 @@ $('#lang').on('click',function(){
   })
 
 
-
-  // let listOfUsers = document.querySelectorAll('#list-of-Users li');
-
-  // listOfUsers.forEach(function(userItem) {
-  //     userItem.addEventListener('click',function(){
-  //         let usersChilds = this.childNodes;
-  //       //  console.log(usersChilds[1]);
-  //         usersChilds.forEach(function(item){
-  //           console.log(item);
-  //         })
-  //       })
-  // });
-
-
-
-
+  // to get clicke item value from add client 
   $( "#list-of-Users li" ).each(function( index ) {
     $(this).on('click',function(){
       let client_title = document.getElementById('client_title');
@@ -60,3 +44,12 @@ $('#lang').on('click',function(){
       
     })
   });
+
+  let tl3 = new TimelineMax();
+  tl3.fromTo('.billdropdown .item-content',1,{opacity: 0,position:'relative',bottom:'-30px'}, {opacity: 1,position:'relative',bottom:0,stagger: 0.1})
+  tl3.reversed(true);
+  $('#menuIcon').on('click',function(){
+    tl3.reversed(!tl3.reversed());
+  })
+  // Dropdownmenu animation 
+
