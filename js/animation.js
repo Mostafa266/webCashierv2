2d382b6664bf910ea0e3bb7bc58b13830555264e
DESKTOP-VC20UED\MostafaAbdelazeem <mostafa.abdo.ch12@gmail.com>
2021-03-11 03:01:51 +0200

    Adjust list-of-users menu in Bill-cash Page

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,5 @@
 let tl = new TimelineMax();
-tl.fromTo('.item-nav', 0.5, { opacity: 0,position:'relative',right:'-20px'}, {opacity: 1,position:'relative',right:0,stagger: 0.1})
-.fromTo('.card', 0.3, {opacity: 0,position:'relative',bottom:'-20px'}, {opacity: 1,position:'relative',bottom:0,stagger: 0.1},'-=1')
+tl.fromTo('.card', 0.3, {opacity: 0,position:'relative',bottom:'-20px'}, {opacity: 1,position:'relative',bottom:0,stagger: 0.1},'-=1')
 .fromTo('.divider', 0.5, {opacity: 0,position:'relative',width:'0'}, {opacity: 1,position:'relative',width:'100%',stagger: 0.1},'-=2.5')
 .fromTo('.billdivider', 0.5, {opacity: 0,position:'relative',width:'0'}, {opacity: 1,position:'relative',width:'100%',stagger: 0.1},'-=1.5')
 .fromTo('.clientContainer', 0.5, {opacity: 0,top:'100px'}, {opacity: 1,top:'56px'},'-=1.45')
@@ -8,23 +7,23 @@ tl.fromTo('.item-nav', 0.5, { opacity: 0,position:'relative',right:'-20px'}, {op
 $('#lang').on('click',function(){
     $('body').toggleClass('ltr');
     tl.restart()
-  })
+});
+
+
+  
+
     // add client list animation
   let tl2 = new TimelineMax({paused:true});
   tl2.to('#productsList',0.3,{opacity:'0',display:'none'})
   tl2.fromTo('#clientList',0.3,{display:'none',opacity:'0'},{display:'block',opacity:1})
   tl2.fromTo('.headerAddClient',0.3,{opacity:0,top:'40px'},{opacity:1,top:"4px"})
   tl2.fromTo('#list-of-Users li',0.3,{display:'none',opacity:'0',bottom:'-30px'},{display:'block',opacity:1,bottom:'0px',stagger:0.1},'-=0.3')
-
   $('#dropdownClient').on('click',function(){
     tl2.play();
-    $('.bill').fadeOut();
     $('.bg-bills').addClass('fullAddClientScreen')
   });
-
   $('#closeClientsList').on('click',function(){
     $('.bg-bills').removeClass('fullAddClientScreen');
-    
     tl2.reverse(0.7);
   })
 
