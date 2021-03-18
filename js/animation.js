f6227b2d28c64dd53e9a22620f0aae9e5035a1f8
DESKTOP-VC20UED\MostafaAbdelazeem <mostafa.abdo.ch12@gmail.com>
2021-03-18 07:58:59 +0200

    Convert GSAP Timelines to JS

--- js/animation.js
+++ js/animation.js
@@ -1,31 +1,50 @@
 $('#removeClientName').fadeOut();
+
+
 let client_title = document.getElementById('client_title');
 let dropdownClient = document.getElementById('dropdownClient');
+
 let tl = new TimelineMax();
-tl.fromTo('.sidebar ul li', 0.5, { opacity: 0,position:'relative',right:'-70px'}, {opacity: 1,position:'relative',right:0,stagger: 0.1})
-.fromTo('.salesContainer .card', 0.3, {opacity: 0,position:'relative',bottom:'-20px'}, {opacity: 1,position:'relative',bottom:0,stagger: 0.1},'-=1')
-.fromTo('.divider', 0.5, {opacity: 0,position:'relative',width:'0'}, {opacity: 1,position:'relative',width:'100%',stagger: 0.1},'-=2.5')
-.fromTo('.billdivider', 0.5, {opacity: 0,position:'relative',width:'0'}, {opacity: 1,position:'relative',width:'100%',stagger: 0.1},'-=1.5')
-.fromTo('.clientContainer', 0.5, {opacity: 0,top:'100px'}, {opacity: 1,top:'56px'},'-=4.45')
-.fromTo('.billDetails', 0.5, {opacity: 0,position:'relative',bottom:'-20px'}, {opacity: 1,position:'relative',bottom:0,stagger: 0.1},'-=4.45')
+
+let sidebarItems = document.querySelectorAll('.sidebar ul li');
+let salesCard = document.querySelectorAll('.salesContainer .card');
+let divider = document.querySelectorAll('.divider');
+let billdivider = document.querySelectorAll('.billdivider');
+let clientContainer = document.getElementsByClassName('.clientContainer');
+let billDetails = document.querySelectorAll('.billDetails');
+let productsList = document.getElementById('productsList');
+let clientList = document.getElementById('clientList');
+let listOfUsers = document.querySelectorAll('#list-of-Users li');
+let headerAddClient = document.getElementsByClassName('.headerAddClient');
+
+
+tl.fromTo(sidebarItems, 0.5, { opacity: 0,position:'relative',right:'-70px'}, {opacity: 1,position:'relative',right:0,stagger: 0.1})
+.fromTo(salesCard, 0.3, {opacity: 0,position:'relative',bottom:'-20px'}, {opacity: 1,position:'relative',bottom:0,stagger: 0.1},'-=1')
+.fromTo(divider, 0.5, {opacity: 0,position:'relative',width:'0'}, {opacity: 1,position:'relative',width:'100%',stagger: 0.1},'-=2.5')
+.fromTo(billdivider, 0.5, {opacity: 0,position:'relative',width:'0'}, {opacity: 1,position:'relative',width:'100%',stagger: 0.1},'-=1.5')
+.fromTo(clientContainer, 0.5, {opacity: 0,top:'100px'}, {opacity: 1,top:'56px'},'-=4.45')
+.fromTo(billDetails, 0.5, {opacity: 0,position:'relative',bottom:'-20px'}, {opacity: 1,position:'relative',bottom:0,stagger: 0.1},'-=4.45')
+
+
 $('#lang').on('click',function(){
     $('body').toggleClass('ltr');
     tl.restart()
   })
+
+
     // add client list animation
   let tl2 = new TimelineMax({paused:true});
-  tl2.to('#productsList',0.3,{opacity:'0',display:'none'})
-  tl2.fromTo('#clientList',0.3,{display:'none',opacity:'0'},{display:'block',opacity:1})
-  tl2.fromTo('#list-of-Users li',0.3,{display:'none',opacity:'0',bottom:'-30px'},{display:'block',opacity:1,bottom:'0px',stagger:0.1},'-=0.3')
-  tl2.fromTo('.headerAddClient',0.3,{opacity:0,top:'40px'},{opacity:1,top:"4px"},'-=1.6')
+  tl2.to(productsList,0.3,{opacity:'0',display:'none'})
+  tl2.fromTo(clientList,0.3,{display:'none',opacity:'0'},{display:'block',opacity:1})
+  tl2.fromTo(listOfUsers,0.3,{display:'none',opacity:'0',bottom:'-30px'},{display:'block',opacity:1,bottom:'0px',stagger:0.1},'-=0.3')
+  tl2.fromTo(headerAddClient,0.3,{opacity:0,top:'40px'},{opacity:1,top:"4px"},'-=1.6')
+
+
 
   $('#dropdownClient').on('click',function(){
     tl2.play();
     $('.bill').fadeOut();
     $('.bg-bills').addClass('fullAddClientScreen')
-    
-
-
   });
 
   $('#closeClientsList').on('click',function(){
@@ -98,7 +117,6 @@ $('#lang').on('click',function(){
           instance.setContent(gettingTitleValue);
           const box = instance.popper.firstElementChild;
           box.classList.add('tippyStyle');
-          console.log(box);
         },
         allowHTML: true,
         animation: 'scale'  
