6bdec15e770b1a5de17685722e6edce97440346a
DESKTOP-VC20UED\MostafaAbdelazeem <mostafa.abdo.ch12@gmail.com>
2021-03-25 01:37:25 +0200

    Add transition between tabs

--- js/pages/sales.module.js
+++ js/pages/sales.module.js
@@ -1,1 +1,19 @@
 import '../components/layoutTabs.module.js';
+
+let clientsList = document.getElementById('clientsList');
+let salesInvoice = document.getElementById('salesInvoice');
+let invoice_search_input_container = document.getElementById('invoice_search_input_container');
+let backBtn = document.getElementById('backBtn');
+
+let tl = new TimelineMax({paused:true});
+
+tl.to(salesInvoice,0.4,{opacity:0,display:'none'})
+.to(clientsList,0.4,{opacity:1,display:'block'})
+
+
+invoice_search_input_container.addEventListener('click',()=>{
+    tl.play()
+})
+backBtn.addEventListener('click',()=>{
+    tl.reverse()
+})
