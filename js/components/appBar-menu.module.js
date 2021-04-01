c521d253598770947d9ca5592012ccc86439dbdd
DESKTOP-VC20UED\MostafaAbdelazeem <mostafa.abdo.ch12@gmail.com>
2021-04-01 19:57:29 +0200

    Adjust animation in menu

--- js/components/appBar-menu.module.js
+++ js/components/appBar-menu.module.js
@@ -1,11 +1,12 @@
 let appbar_menu_btn = document.querySelector('.appbar-menu-btn');
 let appbar_menu = document.querySelector('.appbar-menu');
+let appbar_menuItems = document.querySelectorAll('.appbar-menu ul li');
 
 
 let tl = new TimelineMax({
     paused: true
 });
-tl.fromTo(appbar_menu, 0.3, {
+tl.fromTo(appbar_menu, 0.1, {
     display: 'none',
     opacity: 0
 }, {
@@ -13,7 +14,20 @@ tl.fromTo(appbar_menu, 0.3, {
     opacity: 1,
   
 })
-tl.reverse()
+.fromTo(appbar_menuItems, 0.3, {
+
+    opacity: 0,
+    top:'4rem',
+    position:'relative'
+}, {
+    position:'relative',
+    opacity: 1,
+    top:"0rem",
+    stagger:0.1,
+    ease:Back.easeOut.config(1.7)
+  
+},'-=0.1')
+tl.reverse(0.5)
 appbar_menu_btn.addEventListener('click', () => {
     tl.reversed(!tl.reversed())
 })
@@ -23,7 +37,8 @@ let itemsClick = document.querySelectorAll('.appbar-menu ul li');
 
 itemsClick.forEach(element => {
     element.addEventListener('click',()=>{
-tl.reverse()
+    tl.reverse(0.9)
         
     })
 });
+
