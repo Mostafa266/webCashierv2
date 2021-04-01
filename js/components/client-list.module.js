960d58a2731ecc1e37e34bed0bc434f176648802
DESKTOP-VC20UED\MostafaAbdelazeem <mostafa.abdo.ch12@gmail.com>
2021-04-01 20:32:57 +0200

    Adjust input click with remove value click

--- js/components/client-list.module.js
+++ js/components/client-list.module.js
@@ -8,6 +8,7 @@ let clientsList = document.getElementById('clientsList');
 let salesInvoice = document.getElementById('salesInvoice');
 
 let invoice_search_input_container = document.getElementById('invoice_search_input_container');
+let invoiceInput = document.getElementById('invoiceInput');
 let backBtn = document.getElementById('backBtn');
 
 
@@ -16,19 +17,26 @@ export let tl = new TimelineMax({paused:true});
 tl.to(salesInvoice,0.4,{opacity:0,display:'none'})
 .to(clientsList,0.4,{opacity:1,display:'block'})
 
-
-invoice_search_input_container.addEventListener('click',()=>{
-    tl.play()
+invoiceInput.addEventListener('click',()=>{
+    tl.play();
+    console.log('asd');
 })
 backBtn.addEventListener('click',()=>{
     tl.reverse()
 });
 clientsListUsersItem.forEach(element => {
     element.addEventListener('click',()=>{
-        let invoiceInput = document.getElementById('invoiceInput');
         let getName = element.children[0].children[1].children[0].textContent;
         let getNumber = element.children[0].children[1].children[1].textContent;
         invoiceInput.value = getName + ' - ' + getNumber
-        tl.reverse(0.5)
+        tl.reverse(0.5);
+        invoice_search_input_container.classList.add('have_value')
+        // invoice_search_input_container.classList.add('preventClick')
+   
     })
 });
+
+let remove_search_input = document.getElementById('remove_search_input');
+    remove_search_input.addEventListener('click',()=>{
+        invoiceInput.value = ''
+    })
