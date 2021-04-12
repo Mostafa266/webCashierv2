761b14bfab29d9a437244ee31c6bb67e2c9cd4b8
DESKTOP-VC20UED\MostafaAbdelazeem <mostafa.abdo.ch12@gmail.com>
2021-04-13 01:53:44 +0200

    Create invoice recovery page

---
+++ js/pages/invoices-recovery.module.js
@@ -0,0 +1,61 @@
+import '../components/layoutTabs.module.js';
+// to enable popup and positioning popup 
+import '../components/modal.module.js';
+// to enbale menu in appbar
+import '../components/appBar-menu.module.js';
+
+
+let tl3 = new TimelineMax({paused:true});
+let productsListContainer = document.querySelector('.products-list-container')
+tl3.to(productsListContainer,0.3,{opacity:'0'})
+
+
+document.getElementById('btn--receive-cash').addEventListener('click',(e)=>{
+    e.preventDefault()
+    iziToast.show({
+        title: 'تم بنجاح',
+        message: 'تم استلام المبلغ بنجاح',
+        color:'green',
+        transitionIn:'fadeInUp',
+    });
+})
+
+
+
+
+
+let selectAllBtn = document.getElementById('selectAllBtn');
+
+
+
+
+
+
+
+// Start animation for All Cards in Homepage
+
+let cards = document.querySelectorAll('.product-card');
+
+
+let productItemInList = document.querySelectorAll('.product-item-in-list');
+
+let tl1 = new TimelineMax({paused:true});
+
+
+tl1.fromTo(cards,0.5,{opacity:0,top:'5rem'},{opacity:1,top:'0rem',stagger:0.08,ease:Back.easeOut.config(1.7)})
+.fromTo(productItemInList,0.5,{opacity:0,top:'5rem'},{opacity:1,top:'0rem',stagger:0.08,ease:Back.easeOut.config(1.7)},'-=1.5')
+tl1.play()
+
+
+cards.forEach(card => {
+    card.addEventListener('click',()=>{
+        card.classList.toggle('selected')
+    })
+})
+selectAllBtn.addEventListener('click',()=>{
+    cards.forEach(card => {
+            card.classList.toggle('selected')
+    })
+    selectAllBtn.classList.toggle('clicked')
+})
+
